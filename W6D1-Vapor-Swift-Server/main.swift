cbe32e5c8f789e8414bc72c5790043eb10fc9754
Daniel Mathews <dannyl.mathews@gmail.com>
2016-06-02 14:58:02 -0700

    Initial Commit

---
+++ W6D1-Vapor-Swift-Server/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  W6D1-Vapor-Swift-Server
+//
+//  Created by Daniel Mathews on 2016-06-02.
+//  Copyright © 2016 Daniel Mathews. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
